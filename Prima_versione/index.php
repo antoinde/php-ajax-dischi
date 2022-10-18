a1c573b12e2cdcbb9e106f238772ca3bcf8cf4cc
antoinde <antonello.indelicato98@gmail.com>
2022-10-19 00:13:57 +0200

    Prima versione completata

---
+++ Prima_versione/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- LINK TO MY CSS -->
+    <link rel="stylesheet" href="style/style.css">
+    <!-- TITOLO ESERCIZIO -->
+    <title>PHP Dischi</title>
+</head>
+
+<body>
+    <div class="titolo">
+        <h1> I MIEI ALBUM </h1>
+    </div>
+    <div class="card-container">
+        <?php
+            // Carico il mio database
+            include __DIR__ . '/database.php';
+
+            // Ciclo e stampo tutti gli album
+            foreach($database as $album) {
+                ?>
+                    <!-- Prototipo della card -->
+                    <div class="card">
+
+                        <!-- Parte superiore della card -->
+                        <div class="card-img-container">
+                            <img src="<?= $album['poster'] ?>" alt="<?= $album['title'] ?>" />
+                        </div>
+                        <!-- Parte inferiore della card -->
+                        <div class="card-info-container">
+                            <h3> <?= $album['title'] ?> </h3>
+                            <p> <?= $album['author'] ?> </p>
+                            <p> <?= $album['year'] ?> </p>
+                            <p> <?= $album['genre'] ?> </p>
+                        </div>
+                    </div>
+                <?php
+            }
+        ?>
+    </div>
+
+</body>
+</html>
